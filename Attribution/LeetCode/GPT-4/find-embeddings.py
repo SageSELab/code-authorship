8c0ebbd138635e42b3f7ba31ab08ac65d3a56a04
Atish Kumar Dipongkor <atish@192.168.1.3>
2024-08-02 18:10:40 +0600

    artifacts added for Attribution

---
+++ Attribution/LeetCode/GPT-4/find-embeddings.py
@@ -0,0 +1,42 @@
+
+import pandas as pd
+import numpy as np
+
+df_train = pd.read_csv(f'../data/fold_0_train.csv')
+df_test = pd.read_csv(f'../data/fold_0_test.csv')
+df = pd.concat([df_train, df_test])
+
+
+X = np.load('./gpt_X.npy', allow_pickle=True)
+Y = np.load('./gpt_Y.npy', allow_pickle=True)
+
+df['X'] = X.tolist()
+df['Y'] = Y.tolist()
+
+for fold in range(8):
+    df_train = pd.read_csv(f'../data/fold_{fold}_train.csv')
+    df_test = pd.read_csv(f'../data/fold_{fold}_test.csv')
+    
+    df_train['X'] = df_train['code'].apply(lambda x: df[df['code']== x]['X'].values[0])
+    df_train['Y'] = df_train['code'].apply(lambda x: df[df['code']== x]['Y'].values[0])
+    
+    train_X = np.vstack(df_train['X'].values)
+    train_Y = df_train['Y'].values
+    
+    np.save(f'./gemini_fold_{fold}_X_train.npy', train_X)
+    np.save(f'./gemini_fold_{fold}_Y_train.npy', train_Y)
+    
+    #test
+    
+    df_test['X'] = df_test['code'].apply(lambda x: df[df['code']== x]['X'].values[0])
+    df_test['Y'] = df_test['code'].apply(lambda x: df[df['code']== x]['Y'].values[0])
+    
+    test_X = np.vstack(df_test['X'].values)
+    test_Y = df_test['Y'].values
+    
+    np.save(f'./gemini_fold_{fold}_X_test.npy', test_X)
+    np.save(f'./gemini_fold_{fold}_Y_test.npy', test_Y)
+
+
+
+
--- README.md
+++ README.md
@@ -1,11 +1,14 @@
-### Code Authorship Verification
+### Code Authorship Attribution:
+To solve this problem using Large Language Models (LLMs), we fine-tune five encoder-only LLMsS such as CodeBERT, ContraBERT_C, ContraBERT_G, GraphCodeBERT, and UnixCoder. These models are fine-tuned on the datasets located in the `Datasets` directory.
+
+### Code Authorship Verification:
 The methodology of our code authorship verification is as follows:
 
 ![](./Verification/AV-Methodology.png)
 
 `Verification` folder contains the all data and code for the all experiments we have conducted. To replicate our results, go to `Verification` directory -
 
-- To verify code authorship using Gemini, run `python3 av_gemini.py {dataset_name} {sample}`. `{dataset_name}` should be replaced with the any of the datasets name. The value of `{sample}` should be `positive` or `negative`.
+- To verify code authorship using Gemini, run `python3 av_gemini.py {dataset_name} {sample}`. `{dataset_name}` should be replaced with the any of the datasets name found in `Datasets` folder. The value of `{sample}` should be `positive` or `negative`.
 
 - Similarly, run `python3 av_gpt-4.py {dataset_name} {sample}` to verify code authorship using GPT-4.
 
